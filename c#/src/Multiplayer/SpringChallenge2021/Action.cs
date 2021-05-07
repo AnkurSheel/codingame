634da5cadb9477b6e2bf9f58760df561e95d2dc3
Ankur Sheel <ankur.sheel@gmail.com>
2021-05-07 23:37:30 +1200

    add Seed Action

--- c#/src/Multiplayer/SpringChallenge2021/Action.cs
+++ c#/src/Multiplayer/SpringChallenge2021/Action.cs
@@ -4,7 +4,6 @@ namespace SpringChallenge2021
 {
     internal class Action
     {
-        private const string WAIT = "WAIT";
         private const string SEED = "SEED";
         private const string GROW = "GROW";
         private const string COMPLETE = "COMPLETE";
@@ -12,53 +11,13 @@ namespace SpringChallenge2021
         public static IAction Parse(string action)
         {
             var parts = action.Split(" ");
-            switch (parts[0])
+            return parts[0] switch
             {
-                case GROW:
-                    return new GrowAction(int.Parse(parts[1]));
-                case COMPLETE:
-                    return new CompleteAction(int.Parse(parts[1]));
-                case WAIT:
-                case SEED:
-                default:
-                    return new WaitAction();
-            }
-        }
-
-        private readonly string _type;
-        private readonly int _targetCellIdx;
-        private readonly int _sourceCellIdx;
-
-        public Action(string type, int sourceCellIdx, int targetCellIdx)
-        {
-            _type = type;
-            _targetCellIdx = targetCellIdx;
-            _sourceCellIdx = sourceCellIdx;
-        }
-
-        public Action(string type, int targetCellIdx)
-            : this(type, 0, targetCellIdx)
-        {
-        }
-
-        public Action(string type)
-            : this(type, 0, 0)
-        {
-        }
-
-        public override string ToString()
-        {
-            if (_type == WAIT)
-            {
-                return WAIT;
-            }
-
-            if (_type == SEED)
-            {
-                return $"{SEED} {_sourceCellIdx} {_targetCellIdx}";
-            }
-
-            return $"{_type} {_targetCellIdx}";
+                SEED => new SeedAction(int.Parse(parts[1]), int.Parse(parts[2])),
+                GROW => new GrowAction(int.Parse(parts[1])),
+                COMPLETE => new CompleteAction(int.Parse(parts[1])),
+                _ => new WaitAction()
+            };
         }
     }
 }
---
+++ c#/src/Multiplayer/SpringChallenge2021/Actions/SeedAction.cs
@@ -0,0 +1,19 @@
+﻿namespace SpringChallenge2021.Actions
+{
+    public class SeedAction : IAction
+    {
+        private readonly int _srcTreeIndex;
+        private readonly int _seedIndex;
+
+        public SeedAction(int srcTreeIndex, int seedIndex)
+        {
+            _srcTreeIndex = srcTreeIndex;
+            _seedIndex = seedIndex;
+        }
+
+        public string GetOutputAction()
+        {
+            return $"SEED {_srcTreeIndex} {_seedIndex}";
+        }
+    }
+}
