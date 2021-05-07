624beeac9aae93547e5d7299e6b6ffbf8969978b
Ankur Sheel <ankur.sheel@gmail.com>
2021-05-07 23:03:34 +1200

    Wood 2 -> Wood 1

--- c#/src/Multiplayer/SpringChallenge2021/FinalOutput/Output.cs
+++ c#/src/Multiplayer/SpringChallenge2021/FinalOutput/Output.cs
@@ -1,32 +1,39 @@
+
+
+
+using SpringChallenge2021.Actions;
 using System.Collections.Generic;
 using System.Linq;
+using SpringChallenge2021.Common.Services;
+using SpringChallenge2021.Models;
 using System;
+using System.IO;
 
 
- // 07/05/2021 06:54
+ // 07/05/2021 10:59
+
 
 namespace SpringChallenge2021
 {
     internal class Action
     {
-        public const string WAIT = "WAIT";
-        public const string SEED = "SEED";
-        public const string GROW = "GROW";
-        public const string COMPLETE = "COMPLETE";
+        private const string WAIT = "WAIT";
+        private const string SEED = "SEED";
+        private const string GROW = "GROW";
+        private const string COMPLETE = "COMPLETE";
 
-        public static Action Parse(string action)
+        public static IAction Parse(string action)
         {
             var parts = action.Split(" ");
             switch (parts[0])
             {
-                case WAIT:
-                    return new Action(WAIT);
-                case SEED:
-                    return new Action(SEED, int.Parse(parts[1]), int.Parse(parts[2]));
-                case GROW:
                 case COMPLETE:
+                    return new CompleteAction(int.Parse(parts[1]));
+                case WAIT:
+                case SEED:
+                case GROW:
                 default:
-                    return new Action(parts[0], int.Parse(parts[1]));
+                    return new WaitAction();
             }
         }
 
@@ -67,65 +74,54 @@ namespace SpringChallenge2021
         }
     }
 }
-namespace SpringChallenge2021
-{
-    internal class Cell
-    {
-        private int _index;
-        private int _richness;
-        private int[] _neighbours;
-
-        public Cell(int index, int richness, int[] neighbours)
-        {
-            _index = index;
-            _richness = richness;
-            _neighbours = neighbours;
-        }
-    }
-}
 
 namespace SpringChallenge2021
 {
     internal class Game
     {
-        public int day;
-        public int nutrients;
-        public List<Cell> board;
-        public readonly List<Action> possibleActions;
-        public List<Tree> trees;
-        public int mySun, opponentSun;
-        public int myScore, opponentScore;
-        public bool opponentIsWaiting;
+        private int _day;
+        private int _nutrients;
+        private readonly List<Cell> _board;
+        private readonly List<IAction> _possibleActions;
+        private readonly List<Tree> _trees;
+        private readonly Player _myPlayer;
+        private readonly Player _opponentPlayer;
 
         public Game()
         {
-            board = new List<Cell>();
-            possibleActions = new List<Action>();
-            trees = new List<Tree>();
+            _board = new List<Cell>();
+            _possibleActions = new List<IAction>();
+            _trees = new List<Tree>();
+            _myPlayer = new Player();
+            _opponentPlayer = new Player();
+
+            GenerateBoard();
         }
 
-        public Action GetNextAction()
+        public void ReadGameState()
         {
-            // TODO: write your algorithm here
-            return possibleActions.First();
+            _day = int.Parse(Io.ReadLine()); // the game lasts 24 days: 0-23
+            _nutrients = int.Parse(Io.ReadLine()); // the base score you gain from the next COMPLETE action
+
+            _myPlayer.Parse();
+            _opponentPlayer.Parse();
+
+            ReadTrees();
+            ReadPossibleActions();
         }
-    }
-}
 
-namespace SpringChallenge2021
-{
-    internal class Player
-    {
-        private static void Main(string[] args)
+        public IAction GetNextAction()
         {
-            string[] inputs;
+            var completeAction = _possibleActions.FirstOrDefault(x => x is CompleteAction);
+            return completeAction ?? new WaitAction();
+        }
 
-            var game = new Game();
-
-            var numberOfCells = int.Parse(Console.ReadLine()); // 37
+        private void GenerateBoard()
+        {
+            var numberOfCells = int.Parse(Io.ReadLine()); // 37
             for (var i = 0; i < numberOfCells; i++)
             {
-                inputs = Console.ReadLine().Split(' ');
+                var inputs = Io.ReadLine().Split(' ');
                 var index = int.Parse(inputs[0]); // 0 is the center cell, the next cells spiral outwards
                 var richness = int.Parse(inputs[1]); // 0 if the cell is unusable, 1-3 for usable cells
                 var neigh0 = int.Parse(inputs[2]); // the index of the neighbouring cell for each direction
@@ -135,53 +131,167 @@ namespace SpringChallenge2021
                 var neigh4 = int.Parse(inputs[6]);
                 var neigh5 = int.Parse(inputs[7]);
                 int[] neighs = {neigh0, neigh1, neigh2, neigh3, neigh4, neigh5};
-                var cell = new Cell(index, richness, neighs);
-                game.board.Add(cell);
+                var cell = new Cell(index, (SoilQuality) richness, neighs);
+                _board.Add(cell);
             }
+        }
+
+        private void ReadTrees()
+        {
+            Io.Debug("Reading Trees");
+
+            _trees.Clear();
+            var numberOfTrees = int.Parse(Io.ReadLine()); // the current amount of trees
+            for (var i = 0; i < numberOfTrees; i++)
+            {
+                var inputs = Io.ReadLine().Split(' ');
+                var cellIndex = int.Parse(inputs[0]); // location of this tree
+                var size = int.Parse(inputs[1]); // size of this tree: 0-3
+                var isMine = inputs[2] != "0"; // 1 if this is your tree
+                var isDormant = inputs[3] != "0"; // 1 if this tree is dormant
+                var tree = new Tree(cellIndex, size, isMine, isDormant);
+                _trees.Add(tree);
+            }
+        }
+
+        private IAction ReadPossibleActions()
+        {
+            Io.Debug("Getting next action");
+
+            _possibleActions.Clear();
+            var numberOfPossibleMoves = int.Parse(Io.ReadLine());
+            for (var i = 0; i < numberOfPossibleMoves; i++)
+            {
+                var possibleMove = Io.ReadLine();
+                _possibleActions.Add(Action.Parse(possibleMove));
+            }
+
+            return GetNextAction();
+        }
+    }
+}
+
+namespace SpringChallenge2021
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Io.Initialize();
+
+            var game = new Game();
+
 
             // game loop
             while (true)
             {
-                game.day = int.Parse(Console.ReadLine()); // the game lasts 24 days: 0-23
-                game.nutrients = int.Parse(Console.ReadLine()); // the base score you gain from the next COMPLETE action
-                inputs = Console.ReadLine().Split(' ');
-                game.mySun = int.Parse(inputs[0]); // your sun points
-                game.myScore = int.Parse(inputs[1]); // your current score
-                inputs = Console.ReadLine().Split(' ');
-                game.opponentSun = int.Parse(inputs[0]); // opponent's sun points
-                game.opponentScore = int.Parse(inputs[1]); // opponent's score
-                game.opponentIsWaiting = inputs[2] != "0"; // whether your opponent is asleep until the next day
-
-                game.trees.Clear();
-                var numberOfTrees = int.Parse(Console.ReadLine()); // the current amount of trees
-                for (var i = 0; i < numberOfTrees; i++)
-                {
-                    inputs = Console.ReadLine().Split(' ');
-                    var cellIndex = int.Parse(inputs[0]); // location of this tree
-                    var size = int.Parse(inputs[1]); // size of this tree: 0-3
-                    var isMine = inputs[2] != "0"; // 1 if this is your tree
-                    var isDormant = inputs[3] != "0"; // 1 if this tree is dormant
-                    var tree = new Tree(cellIndex, size, isMine, isDormant);
-                    game.trees.Add(tree);
-                }
-
-                game.possibleActions.Clear();
-                var numberOfPossibleMoves = int.Parse(Console.ReadLine());
-                for (var i = 0; i < numberOfPossibleMoves; i++)
-                {
-                    var possibleMove = Console.ReadLine();
-                    game.possibleActions.Add(Action.Parse(possibleMove));
-                }
+                game.ReadGameState();
 
                 var action = game.GetNextAction();
-                Console.WriteLine(action);
+                Io.WriteLine(action.GetOutputAction());
             }
         }
     }
 }
-namespace SpringChallenge2021
+namespace SpringChallenge2021.Actions
 {
-    internal class Tree
+    public class CompleteAction : IAction
+    {
+        private readonly int _index;
+
+        public CompleteAction(int index)
+        {
+            _index = index;
+        }
+
+        public string GetOutputAction()
+        {
+            return $"COMPLETE {_index}";
+        }
+    }
+}
+namespace SpringChallenge2021.Actions
+{
+    public interface IAction
+    {
+        string GetOutputAction();
+    }
+}
+namespace SpringChallenge2021.Actions
+{
+    public class WaitAction : IAction
+    {
+        public string GetOutputAction()
+        {
+            return "WAIT";
+        }
+    }
+}
+namespace SpringChallenge2021.Common
+{
+    public static class Constants
+    {
+        public const bool IsDebugOn = true;
+
+        public const bool IsForInput = false;
+
+        public const bool IsLocalRun = false;
+    }
+}
+namespace SpringChallenge2021.Models
+{
+    internal class Cell
+    {
+        private int _index;
+        private SoilQuality _soilQuality;
+        private int[] _neighbours;
+
+        public Cell(int index, SoilQuality soilQuality, int[] neighbours)
+        {
+            _index = index;
+            _soilQuality = soilQuality;
+            _neighbours = neighbours;
+        }
+    }
+}
+
+namespace SpringChallenge2021.Models
+{
+    public class Player
+    {
+        private int _sunPoints;
+        private int _score;
+        private bool _isWaiting;
+
+        public void Parse()
+        {
+            Io.Debug("Reading Player State");
+            var inputs = Io.ReadLine().Split(' ');
+
+            _sunPoints = int.Parse(inputs[0]);
+            _score = int.Parse(inputs[1]); // your current score
+            _isWaiting = false;
+
+            if (inputs.Length == 3)
+            {
+                _isWaiting = inputs[2] != "0"; // whether your opponent is asleep until the next day
+            }
+        }
+    }
+}
+namespace SpringChallenge2021.Models
+{
+    public enum SoilQuality
+    {
+        Unknown = 0,
+        Low = 1,
+        Medium = 2,
+        High = 3
+    }
+}
+namespace SpringChallenge2021.Models
+{
+    public class Tree
     {
         private int _cellIndex;
         private int _size;
@@ -194,6 +304,60 @@ namespace SpringChallenge2021
             _size = size;
             _isMine = isMine;
             _isDormant = isDormant;
+        }
+    }
+}
+
+namespace SpringChallenge2021.Common.Services
+{
+    public static class Io
+    {
+        private static StreamReader _file;
+
+        public static void Initialize()
+        {
+            if (Constants.IsLocalRun)
+            {
+                _file = new StreamReader(@".\in.txt");
+            }
+        }
+
+        public static void Debug(string output)
+        {
+            if (Constants.IsDebugOn || Constants.IsForInput)
+            {
+                Console.Error.WriteLine(output);
+            }
+        }
+
+        public static void WriteLine(string output)
+        {
+            Console.WriteLine(output);
+        }
+
+        public static string ReadLine()
+        {
+            if (Constants.IsLocalRun)
+            {
+                return _file.ReadLine();
+            }
+            else
+            {
+                var input = Console.ReadLine();
+
+                if (Constants.IsForInput)
+                {
+                    Debug("IN");
+                    Debug(input);
+                    Debug("/IN");
+                }
+                else
+                {
+                    Debug(input);
+                }
+
+                return input;
+            }
         }
     }
 }
