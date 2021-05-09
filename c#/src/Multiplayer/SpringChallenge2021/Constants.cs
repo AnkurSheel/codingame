dedc78cf594b49892b6dc906ce56d61a423a2965
Ankur Sheel <ankur.sheel@gmail.com>
2021-05-09 14:32:50 +1200

    tweak constants

--- c#/src/Multiplayer/SpringChallenge2021/Constants.cs
+++ c#/src/Multiplayer/SpringChallenge2021/Constants.cs
@@ -2,8 +2,8 @@
 {
     public class Constants
     {
-        public const int DayCutOff = 22;
+        public const int DayCutOff = 21;
         public const int MaxDays = 24;
-        public const int MaxLargeTreesToKeep = 3;
+        public const int MaxLargeTreesToKeep = 7;
     }
 }
