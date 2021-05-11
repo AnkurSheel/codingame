a333cd898f82a76ffca2ea13b07714d7096b6585
Ankur Sheel <ankur.sheel@gmail.com>
2021-05-12 11:46:31 +1200

    :lipstick: refactor

---
+++ c#/src/Multiplayer/SpringChallenge2021/Scorers/GrowActionScorer.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using SpringChallenge2021.Actions;
+using SpringChallenge2021.Models;
+
+namespace SpringChallenge2021.Scorers
+{
+    public class GrowActionScorer
+    {
+        public IAction? GetBestGrowAction(Game game)
+        {
+            var growActions = game.PossibleActions.OfType<GrowAction>().ToList();
+            var bestSoilQuality = SoilQuality.Unusable;
+            var bestTreeSize = TreeSize.Seed;
+            var bestGrowAction = growActions.FirstOrDefault();
+            foreach (var growAction in growActions)
+            {
+                var cell = game.Board[growAction.Index];
+                if (game.ShadowsNextDay.ContainsKey(cell))
+                {
+                    var sizeOfTreeCastingShadow = game.ShadowsNextDay[cell];
+                    var sizeOfTreeAfterGrowth = game.Trees[growAction.Index].Size + 1;
+                    if (sizeOfTreeAfterGrowth <= sizeOfTreeCastingShadow)
+                    {
+                        continue;
+                    }
+                }
+
+                var cellTreeSize = game.Trees[growAction.Index].Size;
+
+                if (bestTreeSize < cellTreeSize)
+                {
+                    bestTreeSize = cellTreeSize;
+                    bestGrowAction = growAction;
+                    bestSoilQuality = cell.SoilQuality;
+                }
+                else if (bestTreeSize == cellTreeSize)
+                {
+                    var cellSoilQuality = cell.SoilQuality;
+                    if (bestSoilQuality < cellSoilQuality)
+                    {
+                        bestSoilQuality = cellSoilQuality;
+                        bestGrowAction = growAction;
+                    }
+                }
+            }
+
+            return bestGrowAction;
+        }
+    }
+}
